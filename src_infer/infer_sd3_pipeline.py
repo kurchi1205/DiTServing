d8c650188a2d994f518d2b4ea07173bfcd336a5a
prerana <prerana.chaks@gmail.com>
2024-11-07 05:09:29 +0000

    compiled pipeline

--- src_infer/infer_sd3_pipeline.py
+++ src_infer/infer_sd3_pipeline.py
@@ -1,13 +1,16 @@
 import torch
 from diffusers import StableDiffusion3Pipeline
+import time
 
 
 def load_pipe():
     pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16)
-
     pipe = pipe.to("cuda")
     return pipe
 
+def compile_pipe(pipe):
+    pipe.transformer = torch.compile(pipe.transformer, fullgraph=False, mode="reduce-overhead")
+    return pipe
 
 def infer(pipe, prompt, num_inference_steps):
     image = pipe(
@@ -23,6 +26,13 @@ def infer(pipe, prompt, num_inference_steps):
 
 if __name__=="__main__":
     pipe = load_pipe()
+    compile = True
+    if compile:
+        pipe = compile_pipe(pipe)
     prompt = "a photo of a cat holding a sign that says hello world"
     num_inference_steps = 28
-    infer(pipe, prompt, num_inference_steps)
+    image = infer(pipe, prompt, num_inference_steps)
+    st = time.time()
+    image = infer(pipe, prompt, num_inference_steps)
+    print("Time taken: ", time.time() - st)
+    image.save("../results/base_sd3_image.jpeg")
