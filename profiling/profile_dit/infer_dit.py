56ef2c9bd48640c0637601eeffe2d3583cfd0ee5
prerana <prerana.chaks@gmail.com>
2024-11-12 02:58:57 +0000

    simple profiling

--- profiling/profile_dit/infer_dit.py
+++ profiling/profile_dit/infer_dit.py
@@ -1,19 +1,20 @@
 import torch
 import os
-import nvtx
+
+from nvtx import annotate
 from diffusers import DiTPipeline, DPMSolverMultistepScheduler
 
+@annotate(message="load", color="yellow")
 def load_pipe():   
     pipe = DiTPipeline.from_pretrained("facebook/DiT-XL-2-512", torch_dtype=torch.float16)
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe = pipe.to("cuda")
     return pipe
 
-
+@annotate(message="Gen", color="blue")
 def generate(pipe, prompt, num_inference_steps):
     class_ids = pipe.get_label_ids([prompt])
-    with nvtx.annotate(message="DiT-Profile", color="green"):
-        output = pipe(class_labels=class_ids, num_inference_steps=num_inference_steps)
+    output = pipe(class_labels=class_ids, num_inference_steps=num_inference_steps)
     image = output.images[0]  
     return image
 
@@ -25,3 +26,7 @@ if __name__ == "__main__":
 # sm__throughput.avg.pct_of_peak_sustained_elapsed# ncu --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed python infer_dit.py
 
 # export PATH=$PATH:/usr/local/cuda/NVIDIA-Nsight-Compute-2024.3/target/linux-desktop-glibc_2_11_3-x64
+# nsys profile python infer_dit.py
+# nsys stats report1.nsys-rep -o model_profile_summary
+# nsys profile --trace=nvtx,cuda,osrt -o pipeline_profile python infer_dit.py
+
