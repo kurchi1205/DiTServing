4acab37c454561b6e70c85d0cb797a3c28875622
prerana <prerana.chaks@gmail.com>
2025-07-12 02:41:09 +0000

    script to run inference in original sd3 code

---
+++ stability_sd3_infer/run_inference.sh
@@ -0,0 +1,15 @@
+python inference.py "A vast landscape made entirely of various meats spreads out before the viewer. tender, succulent hills of roast beef, chicken drumstick trees, bacon rivers, and ham boulders create a surreal, yet appetizing scene. the sky is adorned with pepperoni sun and salami clouds" --output_dir /home/DiTServing/assets/10_steps_prompt_1 --steps 10 --seed 50
+python inference.py "pinkfantasybabes, Close-up of a woman's face wearing an ornate, intricate masquerade mask that covers her eyes and upper nose..." --output_dir /home/DiTServing/assets/10_steps_prompt_2 --steps 10 --seed 50
+python inference.py '''colored sketch in the style of ck-ccd, young Asian woman wearing a motorcycle helmet, 
+        long loose platinum hair,
+        sitting on a large powerful motorcycle, leather jacket, sunset, in orange hues''' --output_dir /home/DiTServing/assets/10_steps_prompt_3 --steps 10 --seed 50
+python inference.py '''stars, water, brilliantly
+gorgeous large scale scene,
+a little girl, in the style of
+dreamy realism, light gold
+and amber, blue and pink,
+brilliantly illuminated in the
+background''' --output_dir /home/DiTServing/assets/10_steps_prompt_4 --steps 10 --seed 50
+python inference.py "Cozy Bedroom Scene with Woman Taking a Mirror Selfie Art" --output_dir /home/DiTServing/assets/10_steps_prompt_5 --steps 10 --seed 50
+python inference.py "Serene Young Woman Portrait with Iridescent Shawl Art" --output_dir /home/DiTServing/assets/10_steps_prompt_6 --steps 10 --seed 50
+python inference.py "Whimsical Garden Gnome with Flowers and Butterflies Poster" --output_dir /home/DiTServing/assets/10_steps_prompt_7 --steps 10 --seed 50
