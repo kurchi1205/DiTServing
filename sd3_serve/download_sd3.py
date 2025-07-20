97308e2c250e8d1a93b9551516ae0d1b9a0a83b6
prerana <prerana.chaks@gmail.com>
2025-07-20 02:56:32 +0000

    refactoring old serving code

--- sd3_serve/download_sd3.py
+++
@@ -1,8 +0,0 @@
-import torch
-import json
-from diffusers import StableDiffusion3Pipeline
-
-pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16)
-pipe.save_pretrained("./sd3_model")
-
-
